5b21ccf54c9432b03a65c6d175010b03dc9d0554
Brad Bautista <bradbautista@gmail.com>
2019-12-30 17:06:55 -0500

    Initial commit from Create-Express-App

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+const helmet = require('helmet')
+const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+
+
+console.log(process.env.API_TOKEN)
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+const portNo = 8000
+
+app.listen(portNo, () => {
+    console.log(`Server listening on port ${portNo}`);
+});
